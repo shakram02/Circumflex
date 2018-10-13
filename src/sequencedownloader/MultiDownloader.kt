c7ff3e600ca33d24cd68897cd6951c74db651375
shakram02 <ahmedhamdyau@gmail.com>
2018-10-13 13:07:44 +0200

    skip download if file exists

--- src/sequencedownloader/MultiDownloader.kt
+++ src/sequencedownloader/MultiDownloader.kt
@@ -29,10 +29,19 @@ class MultiDownloader(private val urls: List<URL>, private var directoryPath: Fi
     override fun call(): Void? = synchronized(lock) {
         updateMessage("Downloading...")
         for (index in urls.indices) {
+            completed = index
             val url = urls[index]
-            val filePath = File(directoryPath, url.getFileName()).absolutePath
+            val file = File(directoryPath, url.getFileName())
 
-            currentDownloader = Downloader(url, filePath)
+            if (isPreviouslyDownloaded(url, file)) {
+                updateMessage("Already downloaded: ${file.name}" +
+                        "\nDownload: ${completed + 1} / ${urls.count()}")
+                updateProgress(1.0, 1.0)
+                Thread.sleep(SLEEP_PERIOD)
+                continue
+            }
+
+            currentDownloader = Downloader(url, file.absolutePath)
             currentDownloader.progressProperty().addListener(onWorkerChangeProgress)
             currentDownloader.onFailed = onDownloaderStateChange
             currentDownloader.onCancelled = onDownloaderStateChange
@@ -41,8 +50,6 @@ class MultiDownloader(private val urls: List<URL>, private var directoryPath: Fi
 
             executor.submit(currentDownloader)
             lock.wait() // Wait until download completes
-
-            completed = index + 1
         }
 
         return null
@@ -74,13 +81,31 @@ class MultiDownloader(private val urls: List<URL>, private var directoryPath: Fi
                     updateMessage("Download: ${completed + 1} / ${urls.count()}")
                     updateProgress(1.0, 1.0)
                     lock.notifyAll()
+                    Thread.sleep(SLEEP_PERIOD)
                 }
             }
         }
     }
 
+    private fun isPreviouslyDownloaded(url: URL, downloadDestination: File): Boolean {
+        val openConnection = url.openConnection()
+        val fileSize = openConnection.contentLengthLong
+
+        if (downloadDestination.exists() && downloadDestination.length() == fileSize) {
+            System.err.println("[Download] skip ${downloadDestination.absolutePath}, it's already downloaded")
+            // TODO updateMessage() ?
+            return true
+        }
+
+        return false
+    }
+
     override fun uncaughtException(p0: Thread?, p1: Throwable?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object {
+        private const val SLEEP_PERIOD = 300L
     }
 }
 
